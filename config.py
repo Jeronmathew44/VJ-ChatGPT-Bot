aa1577b3d0f3743c7dcf3ab679e331a0434dece8
Jeronmathew44 <156781377+Jeronmathew44@users.noreply.github.com>
2024-02-12 18:36:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,14 +8,14 @@ def is_enabled(value, default):
     elif value.lower() in ["false", "no", "0", "disable", "n"]:
         return False
     else:
-        return default
+        return 6845693079
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-ADMINS = int(environ.get("ADMINS", ""))
-DB_URI = environ.get("DB_URI", "")
+API_ID = int(environ.get("API_ID", "27215224"))
+API_HASH = environ.get("API_HASH", "688ae67db37f0ae991c3ecb97d73ff0a")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6845693079:AAG290dcjTkuIoFDg8B4R4e7PeIAVmZ6pT0")
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002142022630"))
+ADMINS = int(environ.get("ADMINS", "6200605339"))
+DB_URI = environ.get("DB_URI", "mongodb+srv://jeronmathew691:jeronmathew691@cluster0.2au4ued.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = environ.get("DB_NAME", "chatgptvjbot")
 OPENAI_API = environ.get("OPENAI_API", "")
 AI = is_enabled((environ.get("AI","True")), False)
